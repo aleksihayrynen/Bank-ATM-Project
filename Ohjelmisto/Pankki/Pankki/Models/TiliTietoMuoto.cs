c05a94bcd9e10eb8c77d604ee478db994a3cc341
akinlawrence <115076399+akinlawrence@users.noreply.github.com>
2023-02-02 13:34:33 +0200

    Ohjelmiston tiedostojen luonti

---
+++ Ohjelmisto/Pankki/Pankki/Models/TiliTietoMuoto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pankki.Models
+{
+    internal class TiliTietoMuoto
+    {
+    }
+}
---
+++ Ohjelmisto/Pankki/Pankki/db-pankki.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pankki
+{
+    internal class DBPankki
+    {
+        public DBPankki()
+        {
+            Console.WriteLine("Luokka luotu -> DBPANKKI!");
+        }
+    }
+}
