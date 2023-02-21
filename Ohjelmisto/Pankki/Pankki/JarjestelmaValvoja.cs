ed671505496dbc01c3e616ff7f944ee0513c5651
aleksihayrynen <115076587+aleksihayrynen@users.noreply.github.com>
2023-02-21 14:13:24 +0200

    Admin toimintojen viimeistely

--- Ohjelmisto/Pankki/Pankki/JarjestelmaValvoja.cs
+++ Ohjelmisto/Pankki/Pankki/JarjestelmaValvoja.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.Arm;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,13 +13,12 @@ namespace Pankki
     {
         public JarjestelmaValvoja() {
             /// KOOODI TÄNNE
-            /// 
             Console.WriteLine("Tervetuloa Järjelmävalvoja systeemin scrum master hallitsija!" );
         
             while (true) // ehkä korvattavissa nintendo switchillä
             {
                 Console.Write
-                ("\n\n1: HAE KÄYTTÄJÄ\n2: POISTA KÄYTTÄJÄ\n3: HAE KAIKKI KÄYTTÄJÄTIEDOT\n4: JOTAIN INNOVATIIVISTÄ\n0: Poistu\n= ");
+                ("\n\n1: HAE KÄYTTÄJÄ\n2: POISTA KÄYTTÄJÄ\n3: HAE KAIKKI KÄYTTÄJÄTIEDOT\n4: LISÄÄ KÄYTTÄJÄ\n5: HAE TILI\n0: Poistu\n= ");
                 var vaihtoehto = int.Parse(Console.ReadLine());
 
                 if (vaihtoehto == 0)
@@ -31,7 +31,11 @@ namespace Pankki
                         var kayttajatunnus = Console.ReadLine();
                         var haettu = db_pankki.HaeKayttaja(kayttajatunnus);
                         if (haettu != null)
-                            Console.WriteLine(haettu);
+                        {
+                            Console.WriteLine(haettu.kayttaja + " | " + haettu.etunimi + " | " + haettu.sukunimi + " | " + haettu.pin + " | " + haettu.luontiPaiva);
+                        }
+                             
+                         
                         else
                         {
                             Console.WriteLine("Käyttäjää ei löytynyt");
@@ -49,9 +53,6 @@ namespace Pankki
                         {
                             Console.WriteLine("Ei löytynyt! Epäonnistu pahasti!");
                         }
-
-
-
                         break;
 
                     case 3:
@@ -62,7 +63,38 @@ namespace Pankki
                         }
                         break;
                     case 4:
+                        Console.WriteLine("Syötä etunimi");
+                        var etunimi = Console.ReadLine();
+                        Console.WriteLine("Syötä sukunimi");
+                        var sukunimi = Console.ReadLine();
+                        Console.WriteLine("Syötä 4-numeroinen pin-koodi");
+                        var salasana = Console.ReadLine();
+                        if (salasana.Length > 5)
+                            do
+                            {
+                                Console.WriteLine("Virheellinen salasana(max 4 numeroa)\nSyötä uudestaan: ");
+                                salasana = Console.ReadLine();
+                            } while (salasana.Length > 5);
+
+                        db_pankki.LisaaKayttaja(Salasanahallinta.LuoSuolattuSalasana(salasana), etunimi, sukunimi);
                         break;
+                    case 5:
+                        Console.WriteLine("Syötä tilinumero");
+                        var tilinumero = Console.ReadLine();
+                        var haettuTiliNumero = db_pankki.HaeTili(tilinumero);
+                        if (haettuTiliNumero != null)
+                        {
+                            var aa = db_pankki.HaeKayttaja(tilinumero);
+                            Console.WriteLine(aa.etunimi+" "+aa.sukunimi);
+                            Console.WriteLine(haettuTiliNumero.tiliOmistajakayttaja + " | " + haettuTiliNumero.tiliTunnus + " | " + haettuTiliNumero.saldo + " e");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Tiliä ei löytynyt");
+                        }
+                        break;
+
                     default:
                         break;
                 }
@@ -70,17 +102,3 @@ namespace Pankki
         } 
     }
 }
-
-/*
-db_pankki.PoistaKayttaja(kayttaja);
-db_pankki.LisaaKayttaja( "1234", "janne", "niminen");
-db_pankki.LisaaKayttaja("1534", "jonne", "niminen");
-db_pankki.LisaaKayttaja("salasana", "Aleksi", "Opiskelija");
-var henkilo = db_pankki.HaeKayttaja("Kokeilija");
-db_pankki.HaeKaikkiKayttajat();
-
-db_pankki.Siirto("1234", "Kokeilija", 22.5 );
-db_pankki.Talletus(22.3, aktiivinen.kayttaja);
-db_pankki.Nosto(22.3, aktiivinen.kayttaja);
-
-*/
